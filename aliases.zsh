afc7b60b1aab72298a5b7a03d6b47f8254e35d84
oscarsix <ocarsix@protonmail.ch>
2017-02-10 18:44:17 +0100

    changed aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -2,7 +2,7 @@
 alias dc='docker-compose $@'
 alias dcup='docker-compose up -d'
 alias dcdn='docker-compose down'
-alias dcls='docker-compose logs -f'
+alias dclf='docker-compose logs -f'
 alias dcex='docker-compose exec $@'
 
 alias ppp='puppet agent --enable ; puppet agent -t ; puppet agent --disable'
@@ -10,4 +10,3 @@ alias pppn='puppet agent --enable ; puppet agent -t --noop ; puppet agent --disa
 alias ppon='puppet agent --enable'
 alias ppoff='puppet agent --disable'
 
-alias update_dotfiles='./dotfiles/install'
