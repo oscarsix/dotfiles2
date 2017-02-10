178029b37fc156ceb340f5cb72f4639d7a8bcfaf
oscarsix <ocarsix@protonmail.ch>
2017-02-10 18:47:05 +0100

    changed dotfiles plugin

--- dotfiles.plugin.zsh/dotfiles.plugin.zsh
+++ dotfiles.plugin.zsh/dotfiles.plugin.zsh
@@ -5,9 +5,16 @@
 
 }
 
-edit_aliases(){
+edit_aliases() {
   vim ~/.dotfiles/aliases.zsh
   git -C ~/.dotfiles add ~/.dotfiles/aliases.zsh
   git -C ~/.dotfiles commit -m "changed aliases.zsh"
+  git -C ~/.dotfiles push origin master
+}
+
+edit_dotfiles_plugin() {
+  vim ~/.dotfiles/dotfiles.plugin.zsh/dotfiles.plugin.zsh
+  git -C ~/.dotfiles add ~/.dotfiles/dotfiles.plugin.zsh/dotfiles.plugin.zsh
+  git -C ~/.dotfiles commit -m "changed dotfiles plugin"
   git -C ~/.dotfiles push origin master
 }
