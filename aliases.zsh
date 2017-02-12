bd38223f7da3bd86d792cb21726a41ffa1893ee9
oscarsix <ocarsix@protonmail.ch>
2017-02-12 12:57:52 +0100

    changed aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,6 @@
 #ZSH Aliases
 alias dc='docker-compose $@'
-alias dcup='docker-compose up -d'
+alias dcup='docker-compose up -d $@'
 alias dcdn='docker-compose down'
 alias dclf='docker-compose logs -f'
 alias dcex='docker-compose exec $@'
