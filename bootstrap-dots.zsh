33b5e5dba3dccf45d02256260435f39876620948
Oskar Malnowicz <oskar.malnowicz@rise-world.com>
2017-04-08 11:37:24 +0200

    remove dots folder before clone

--- bootstrap-dots.zsh
+++ bootstrap-dots.zsh
@@ -5,18 +5,10 @@ if [[ $? == 1 ]]; then
     sudo chown -R $(id -u):$(id -g) ~
 fi
 
-if [[ -d dots ]]; then
-  print -P "%F{green}Folder dots already exists.%f"
-  print -P "%F{green}Updating dots.%f"
-  git -C ~/.dotfiles/dots pull --rebase
-  print -P "%F{green}Updating dotfiles.%f"
-  git -C ~/.dotfiles pull --rebase
-  exit 0
-fi
-
 print -P "%F{white}Bootstraping dots.%f"
+rm -rf dots
 
-if [[ $(hostname -f) =~ ".rz01.riseopsi.*$" ]]; then
+if [[ $(hostname -f) =~ ".rz01.riseops.*$" ]]; then
   print -P "Downloading dots for rz01 environment"
   git clone ssh://git@b1t.uk:1848/om/rise-dots.git dots
   exit 0
