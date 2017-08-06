f897a68495721a3cd54ecd305282fe40c0d753b9
oscarsix <ocarsix@protonmail.ch>
2017-08-06 12:21:14 +0200

    updates

--- functions.zsh
+++ functions.zsh
@@ -55,21 +55,22 @@ _git_pull_dotfiles_repo() {
 _bootstrap_dots() {
 	print -P "%F{white}Bootstraping dots.%f"
 	rm -rf dots
+  git clone ssh://om@home.1210.uk:20000/red01/home_om/git/om/dots.git dots
 
-	if [[ $(hostname -f) =~ ".rz01.riseops.*$" ]]; then
-		print -P "Downloading dots for rz01 environment"
-		git clone ssh://om@home.1210.uk:20000/red01/home_om/git/om/rise-dots.git dots
-		exit 0
-	fi
+	# if [[ $(hostname -f) =~ ".rz01.riseops.*$" ]]; then
+	# 	print -P "Downloading dots for rz01 environment"
+	# 	git clone ssh://om@home.1210.uk:20000/red01/home_om/git/om/rise-dots.git dots
+	# 	exit 0
+	# fi
 
-	if [[ $(hostname -f) =~ ".akx.evva.*$" ]]; then
-		print -P "Downloading dots for evva environment"
-		git clone ssh://om@home.1210.uk:20000/red01/home_om/git/om/evva-dots.git dots
-		exit 0
-	fi
+	# if [[ $(hostname -f) =~ ".akx.evva.*$" ]]; then
+	# 	print -P "Downloading dots for evva environment"
+	# 	git clone ssh://om@home.1210.uk:20000/red01/home_om/git/om/evva-dots.git dots
+	# 	exit 0
+	# fi
 
-	print -P "Downloading universal dots"
-	git clone ssh://om@home.1210.uk:20000/red01/home_om/git/om/dots.git dots
+	# print -P "Downloading universal dots"
+	# git clone ssh://om@home.1210.uk:20000/red01/home_om/git/om/dots.git dots
 }
 
 _commit_push_changes() {
--- update_dotfiles.zsh
+++ update_dotfiles.zsh
@@ -16,12 +16,21 @@ if [[ ! -d ~/.ssh/assh.d ]]; then
   mkdir ~/.ssh/assh.d
 fi
 
+if [[ ! -f ~/.bin/assh ]]; then
+  _download_assh  
+fi
+
 _check_dotfiles_last_update
-if [[ $? == 1 ]]; then
+_res=$?
+if [[ $_res == 1 ]]; then
   _git_pull_dotfiles_repo
   _update_dotfiles_submodules  
   _bootstrap_dots
   assh config build > ~/.ssh/config
   _touch_dotfiles_last_update_file
 fi
-
+if [[ $_res == 2 ]]; then
+  _bootstrap_dots
+  assh config build > ~/.ssh/config
+  _touch_dotfiles_last_update_file
+fi
