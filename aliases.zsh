8cf24c5fb13e00638d210640c62d2d110c25ceba
Oskar Malnowicz <oskar.malnowicz@rise-world.com>
2017-09-27 09:48:58 +0200

    changed aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -22,7 +22,7 @@ alias fpm-cook='docker run --rm -ti -v "$(pwd):/build" fpm-cook'
 #
 alias install_climate='sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/adtac/climate/master/install)"'
 
-#alias ssh='assh wrapper ssh'
+alias ssh='assh wrapper ssh'
 
 alias sctl='systemctl $@'
 alias sstart='systemctl start $@'
