3a4b8bd3e5856942089cbfaa2d848239ac51c868
Oskar Malnowicz <oskar.malnowicz@rise-world.com>
2017-10-02 08:47:09 +0200

    changed aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,6 @@
 #ZSH Aliases
+
+# Docker
 alias dc='docker-compose $@'
 alias dcup='docker-compose up -d $@'
 alias dcdn='docker-compose down'
@@ -6,24 +8,26 @@ alias dclf='docker-compose logs -f'
 alias dcex='docker-compose exec $@'
 alias dcre='docker-compose restart $@'
 alias dps='docker ps $@'
+alias fpm-cook='docker run --rm -ti -v "$(pwd):/build" fpm-cook'
 
+# Puppet
 alias ppp='puppet agent --enable ; puppet agent -t ; puppet agent --disable'
 alias pppn='puppet agent --enable ; puppet agent -t --noop ; puppet agent --disable'
 alias ppon='puppet agent --enable'
 alias ppoff='puppet agent --disable'
 
+# Linux
 alias ps_top='ps -eo pcpu,args --sort=-%cpu|head'
 alias tcpl='lsof -iTCP -sTCP:LISTEN -P -n'
-
-##
 alias tar_etc='tar czvf /etc-"$(date +%FT%T)".tar.gz /etc'
-###
-alias fpm-cook='docker run --rm -ti -v "$(pwd):/build" fpm-cook'
-#
+
 alias install_climate='sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/adtac/climate/master/install)"'
 
+# SSH
 alias ssh='assh wrapper ssh'
+alias tor-ssh='ssh -o ProxyCommand='nc -x localhost:9050 %h %p' $@'
 
+# Systemd
 alias sctl='systemctl $@'
 alias sstart='systemctl start $@'
 alias sstop='systemctl stop $@'
